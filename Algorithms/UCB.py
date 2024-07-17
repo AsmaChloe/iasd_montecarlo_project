a8610d3344b7aaf115e598972755a949959a0310
AsmaChloe <asmachloe@gmail.com>
2024-07-18 01:29:41 +0200

    Optimized UCB the same way we optimized UCT

--- Algorithms/UCB.py
+++ Algorithms/UCB.py
@@ -3,6 +3,7 @@ import gym
 import random
 from gym_abalone.envs.abalone_env import AbaloneEnv
 import numpy as np
+import time
 
 class UCBAgent:
     def __init__(self, env, max_iterations=30, exploration_param=1.4):
@@ -11,26 +12,51 @@ class UCBAgent:
         self.exploration_param = exploration_param
 
     def ucb_search(self):
-        current_env = self.env
-        # print(f"\tcurrent_env: {current_env}")
-        possible_moves = self.env.game.get_possible_moves(current_env.game.current_player)
-        #Children and their stats
-        children = {move: {"game":clone_game(current_env.game), "visit_count" : 0, "total_reward" : 0} for move in possible_moves}
+
+        # Get all possible moves as children
+        possible_moves = self.env.game.get_possible_moves(self.env.game.current_player, group_by_type=True)
+        children = { move : {'game' : clone_game(self.env.game),'visit_count' : 0, 'total_reward' : 0, 'move_type' : move_type} for move_type, moves in possible_moves.items() for move in moves}
 
         for _ in range(self.max_iterations):
-            # print(f"\t\titeration {_}")
-            # Select a child move based on UCB
-            selected_move = self.selection(children)
-            # print(f"\t\t\t selected node: {selected_move}")
-            # Simulate the game from the selected move
-            reward = self.simulation(selected_move, children[selected_move])
-            # print(f"\t\t\t reward: {reward}")
-            # Update the stats of the selected move
-            self.update_stats(children[selected_move], reward)
+            print(f"iteration {_}")
 
-        # Choose the action with the highest average reward
-        best_move = max(children.keys(), key=lambda move: children[move]["total_reward"] / children[move]["visit_count"] if children[move]["visit_count"] > 0 else 0)
-        # print(f"\tbest_move: {best_move}")
+            # Selection
+            start_time = time.time()
+            selected_move = self.selection(children)
+            end_time = time.time()
+            print(f"\tSelection time: {end_time - start_time:.6f} seconds")
+
+            # Simulation
+            start_time = time.time()
+            reward = self.simulation(selected_move, children[selected_move])
+            end_time = time.time()
+            print(f"\tSimulation time: {end_time - start_time:.6f} seconds")
+
+            # Updating stats
+            start_time = time.time()
+            self.update_stats(children[selected_move], reward)
+            end_time = time.time()
+            print(f"\tUpdating stats time: {end_time - start_time:.6f} seconds")
+
+        ## Choose the action with the highest average reward
+
+        # Select candidate children with the highest average reward
+        # Move with the highest average reward
+        criterion_move = max(children.keys(), key=lambda move: children[move]["total_reward"] / children[move]["visit_count"] if children[move]["visit_count"] > 0 else 0)
+        moves_by_move_type = {"winner": [], "ejected": [], "inline_push": [], "sidestep_move": [], "inline_move": []}
+
+        # Get all moves with the same average reward as the criterion move
+        for move, move_stats in children.items():
+            avg_reward = children[move]["total_reward"] / children[move]["visit_count"] if children[move]["visit_count"] > 0 else 0
+            if avg_reward == children[criterion_move]["total_reward"] / children[criterion_move]["visit_count"]:
+                moves_by_move_type[children[move]["move_type"]].append(move)
+
+        # Choose the best child among the candidate moves
+        for move_type, moves in moves_by_move_type.items():
+            if len(moves) > 0:
+                best_move = random.choice(moves)
+                break
+
         return best_move
 
     def ucb(self, node, total_visits):
@@ -41,23 +67,28 @@ class UCBAgent:
         return average_reward + exploration_term
 
     def selection(self, children):
-        # Get the total number of visits of all children
+        # As I am computing the UCB value for each child, I update visit_count
         total_visits = sum(node["visit_count"] for node in children.values()) + 1
 
         # Choose the child with the highest UCB value
         ucb_values = {move: self.ucb(node, total_visits) for move, node in children.items()}
-        selected_move = max(ucb_values.keys(), key=lambda move: ucb_values[move])
+        selected_move = max(ucb_values.keys(), key=lambda move: ucb_values[move]) # TODO selectionner en fonction du type
         return selected_move
 
     def simulation(self, selected_move, child):
         sim_state = clone_game(child["game"])
         sim_state.action_handler(selected_move[0], selected_move[1])
+
         while not sim_state.game_over:
-            possible_moves = sim_state.get_possible_moves(sim_state.current_player)
-            if not possible_moves:
+            possible_moves = sim_state.get_possible_moves(sim_state.current_player, group_by_type=True)
+            # No possible moves, end the game
+            if len(possible_moves) == 0:
                 break
-            random_move = random.choice(possible_moves)
-            sim_state.action_handler(random_move[0], random_move[1])
+            for move_type in ['winner', 'ejected', 'inline_push', 'sidestep_move', 'inline_move']:
+                if possible_moves[move_type]:
+                    random_move = random.choice(possible_moves[move_type])
+                    sim_state.action_handler(random_move[0], random_move[1])
+                    break
 
         winner = np.argmax(sim_state.players_victories)
         return 1.0 if winner == child["game"].current_player else 0.0
@@ -73,13 +104,13 @@ def str_game(game):
     return f"Game({game.current_player}, {game.players_victories})"
 
 
-# env = AbaloneEnv(render_mode='human')
-env = AbaloneEnv(render_mode='terminal')
+env = AbaloneEnv(render_mode='human', max_turns=2000)
+# env = AbaloneEnv(render_mode='terminal', max_turns=2000)
 ucb_agent = UCBAgent(env, max_iterations=3)
 
 NB_EPISODES = 1
 for episode in range(1, NB_EPISODES + 1):
-    env.reset(random_player=True, random_pick=True)
+    env.reset(random_player=True, random_pick=False)
     done = False
     while not done:
         action = ucb_agent.ucb_search()
